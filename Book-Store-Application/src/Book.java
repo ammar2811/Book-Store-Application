73d4878c148f2b62a91417a22ccc9e0a0200f0f6
Mon-arc <hassanelshikh25@gmail.com>
2025-03-29 02:21:40 -0400

    Initial implementation of User.java and Book.java.

--- Book-Store-Application/src/Book.java
+++ Book-Store-Application/src/Book.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Hassan
- */
 public class Book {
-    
+
+	private String title;
+	private double price;
+
+	public Book(String title, double price) {
+		this.title = title;
+		this.price = price;
+	}
+
+	public String getDetails() {
+		String str = String.format("Title: %s\nPrice: $%.2f", title, price);
+		return str;
+	}
 }
--- Book-Store-Application/src/User.java
+++ Book-Store-Application/src/User.java
@@ -1,12 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Hassan
- */
 public class User {
-    
+	private String username;
+	private String password;
+	private boolean isLoggedIn;
+
+	public User(String username, String password) {
+		setUsername(username);
+		this.password = password;
+		isLoggedIn = false;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public abstract void login();
+
+	public abstract voidw logout();
 }
