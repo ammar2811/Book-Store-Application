7a1fb74ede0371a4b0528a71c513492318817471
KG-2525 <guled.khalid16@gmail.com>
2025-03-29 21:57:36 -0400

    Update BookStore.java

--- Book-Store-Application/src/BookStore.java
+++ Book-Store-Application/src/BookStore.java
@@ -2,123 +2,170 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
  *
- * @author Khalid Guled
+ * @author Khalid 
+ */
+
+/**
+ * 
+ * Represents a Book store where all book and customer data is stored. could add or remove book/customer
+ * 
  */
 public class BookStore {
-    public List<Book> bookList = new ArrayList<>();
-    public List<Customer> customerList = new ArrayList<>();
-
-    public List<Book> getBooks() {
+    private List<Book> bookList = new ArrayList<>();
+    private List<Customer> customerList = new ArrayList<>();
+    /**
+     * 
+     * @return arrayList containing book data
+     */
+    public List<Book> getBooks(){
         return bookList;
     }
-
-    public List<Customer> getCustomers() {
+    /**
+     * 
+     * @return arrayList containing customer data
+     */
+    public List<Customer> getCustomers(){
         return customerList;
     }
-
+    /**
+     * Adds a book to the BookList array
+     * @param b Book 
+     */
     public void addBook(Book b) {
         bookList.add(b);
     }
-
+    /**
+     * Deletes a book from the BookList array
+     * @param b Book
+     * @return true if book was deleted from arrayList. returns false if book was not deleted.
+     */
     public boolean deleteBook(Book b) {
         return bookList.remove(b);
     }
-
+    /**
+     * Adds a customer to the CustomerList array
+     * @param c Customer
+     */
     public void addCustomer(Customer c) {
         customerList.add(c);
     }
-
+     /**
+     * Deletes a customer from the CustomerList array
+     * @param c Customer
+     * @return true if customer was deleted from arrayList. returns false if customer was not deleted.
+     */
     public boolean deleteCustomer(Customer c) {
         return customerList.remove(c);
     }
-
-    public void StoreBooks() {
+    
+    public void StoreBooks(){
         File bookstext = new File("books.txt");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookstext, false))) {
-            for (Book book : bookList) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookstext,false))){
+            for (Book book : bookList){
                 writer.write(book.getDetails());
                 writer.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            }   
         }
+        catch (IOException e){
+            e.printStackTrace();
+        } 
     }
-
-    public void StoreCustomers() {
+    
+    public void StoreCustomers(){
         File customerstext = new File("customers.txt");
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(customerstext, false))) {
-            for (Customer customer : customerList) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(customerstext,false))){
+            for (Customer customer : customerList){
                 writer.write(customer.getUsername() + "," + customer.getPassword() + "," + customer.getPoints());
                 writer.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            }   
         }
+        catch (IOException e){
+            e.printStackTrace();
+        } 
     }
-
-    public void loadBooks() {
+   /**
+    * Copies the book data stored in books.txt and places it inside of the bookList Array.
+    * needed to convert string data into book data type
+    */
+    public void loadBooks(){
         File bookstext = new File("books.txt");
-
-        if (!bookstext.exists()) {
+        
+        if (!bookstext.exists()){
             System.out.println("books.txt was not found");
             return;
         }
-        try (Scanner scanner = new Scanner(bookstext)) {
-            while (scanner.hasNextLine()) {
+        try(Scanner scanner = new Scanner(bookstext)){
+            while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 String[] details = line.split(",");
-
-                if (details.length == 2) {
+                
+                if (details.length == 2){
                     String title = details[0].trim();
                     double price = Double.parseDouble(details[1].trim());
                     Book book = new Book(title, price);
                     bookList.add(book);
-                } else {
+                }
+                else{
                     System.out.println("Skipping incorrect book entry: " + line);
                 }
             }
         }
-
-        catch (FileNotFoundException e) {
+        
+        catch (FileNotFoundException e){
             System.out.println("Error: File not found");
             e.printStackTrace();
-        } catch (NumberFormatException e) {
+        }
+        catch (NumberFormatException e){
             System.out.println("Error in parsing book price: ensure correct format");
             e.printStackTrace();
         }
     }
-
-    public void loadCustomers() {
+    
+    /**
+    * Copies the customer's data stored in customer.txt and places it inside of the customerList Array.
+    * needed to convert string data into customer data type
+    */
+    public void loadCustomers(){
         File customerstext = new File("customers.txt");
-
-        if (!customerstext.exists()) {
+        
+        if (!customerstext.exists()){
             System.out.println("customers.txt was not found");
             return;
         }
-        try (Scanner scanner = new Scanner(customerstext)) {
-            while (scanner.hasNextLine()) {
+        try(Scanner scanner = new Scanner(customerstext)){
+            while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 String[] details = line.split(",");
-
-                if (details.length == 3) {
+                
+                if (details.length == 3){
                     String username = details[0].trim();
                     String password = details[1].trim();
-                    double points = Double.parseDouble(details[2].trim());
+                    int points = Integer.parseInt(details[2].trim());
                     Customer customer = new Customer(username, password, points);
                     customerList.add(customer);
-                } else {
+                }
+                else{
                     System.out.println("Skipping incorrect customer entry: " + line);
                 }
             }
         }
-
-        catch (FileNotFoundException e) {
+        
+        catch (FileNotFoundException e){
             System.out.println("Error: File not found");
             e.printStackTrace();
-        } catch (NumberFormatException e) {
+        }
+        catch (NumberFormatException e){
             System.out.println("Error in parsing customer Points: ensure correct format");
             e.printStackTrace();
         }
